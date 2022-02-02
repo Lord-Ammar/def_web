1954f47b550450d0eb0667009d82e1f1594b7eb8
Creator Lord-Botz <98245473+Lord-Ammar@users.noreply.github.com>
2022-02-02 12:10:51 +0700

    Add files via upload

---
+++ data/report.py
@@ -0,0 +1,19 @@
+import os,sys,time
+print ("""\033[1;95m ____  ____  ____  _____  ____  ____ 
+(  _ \( ___)(  _ \(  _  )(  _ \(_  _)
+\033[1;97m )   / )__)  )___/ )(_)(  )   /  )(  
+(_)\_)(____)(__)  (_____)(_)\_) (__)""")
+print
+print ("\033[1;96mStatus Report:\033[1;92mOnline")
+print ("Jangan Di Decode Ya Ngab:)")
+print
+print ("Masukkan Masalah Nya Gan?")
+time.sleep(5)
+print ("")
+print ("")
+print ("\033[1;97mContoh:\033[1;95mScript+Nya+Eror+Bwang")
+print ("\033[1;97mTambahkan Tanda\033[1;95m + \033[1;97mUntuk Spasi")
+print ("")
+tanya = input("Mau Report Apa Gan? ")
+print ("\033[1;96mhttps://wa.me/6288229683561?text="+tanya)
+print ("\033[1;97mTampal Text Diatas Ke Google")
