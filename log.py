b0ee5b01c947b2e37556b3b92555ec813009428d
Creator Lord-Botz <98245473+Lord-Ammar@users.noreply.github.com>
2022-02-02 14:05:21 +0700

    Update log.py

--- log.py
+++ log.py
@@ -1,9 +1,11 @@
 import os,sys,time
 localtime = time.asctime(time.localtime(time.time()))
+ip = requests.get('https://api.ipify.org').text
 print
 print (" \033[1;95m[\033[1;90m•\033[1;95m]\033[1;96m Creator\033[1;93m: \033[1;92mAmmarBN && Mr.404rgr")
 print (" \033[1;95m[\033[1;90m•\033[1;95m]\033[1;96m Github\033[1;93m: \033[1;92mgithub.com/AmmarBN")
 print (" \033[1;95m[\033[1;90m•\033[1;95m]\033[1;96m Status\033[1;93m: \033[1;92mSelalu Online")
 print (" \033[1;95m[\033[1;90m•\033[1;95m]\033[1;96m Version Tools\033[1;93m: \033[1;92mv1.0.2")
 print (" \033[1;95m[\033[1;90m•\033[1;95m]\033[1;96m Waktu\033[1;93m: \033[1;92m"+localtime)
+print (" \033[1;95m[\033[1;90m•\033[1;95m]\033[1;96m Your Ip\033[1;93m: \033[1;92m"+ip)
 print
