e531e6aeb084a9c91f8a34b1b297f97eb6844058
Creator Lord-Botz <98245473+Lord-Ammar@users.noreply.github.com>
2022-02-02 14:29:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/data/data/com.termux/files/home/bash
 apt update && apt upgrade -y
 clear
+apt install python -y
 apt install python2 -y
+PIP install requests && pip2 install requests
 clear
 apt install curl -y
 clear
